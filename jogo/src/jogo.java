2d038c89db73c2cb13c70b4f9b8c6f5156734252
lgrando <grandol93@gmail.com>
2013-08-14 19:43:50 -0300

    jogo

--- jogo/src/jogo.java
+++ jogo/src/jogo.java
@@ -1,27 +1,23 @@
 import java.util.Random;
 
 public class jogo {
-
 	public static void main(String[] args) {
-		
 		Random n = new Random();
-		
-		String[] sufixo = {"de Copa","de Espada","de Ouro","de Paus"};
+		String[] sufixo = { "de Copa","de Espada","de Ouro","de Paus" };
+		String[] baralho = { "A","1","2","3","4","5","6","7","8","9","10","J","Q","K" };
 		String naipe = null;
-		int carta = 0;
+		String carta = null;
 		String cartas = null;
 		String[] lista = new String[20];
-		
-		for(int i = 0 ; i < 20 ; i++){
+		for (int i = 0; i < 20; i++) {
 			naipe = sufixo[((int) (sufixo.length * Math.random()))];
-			carta = n.nextInt(13)+1;
-			cartas = Integer.toString(carta)+" "+naipe;
-			lista[i] = cartas;
-		}		
-		
-		System.out.println("(JOGADOR 1): " + lista[0]+" | "+lista[1]+" | "+lista[2]+" | "+lista[3]+" | "+lista[4]);		
-		System.out.println("(JOGADOR 2): " + lista[5]+" | "+lista[6]+" | "+lista[7]+" | "+lista[8]+" | "+lista[9]);
-		System.out.println("(JOGADOR 3): " + lista[10]+" | "+lista[11]+" | "+lista[12]+" | "+lista[13]+" | "+lista[14]);
-		System.out.println("(JOGADOR 4): " + lista[15]+" | "+lista[16]+" | "+lista[17]+" | "+lista[18]+" | "+lista[19]);
+			cartas = baralho[((int) (baralho.length * Math.random()))];
+			carta = cartas + " " + naipe;
+			lista[i] = carta;
+		}
+		System.out.println("(JOGADOR 1): " + lista[0] + " | " + lista[1] + " | " + lista[2] + " | " + lista[3] + " | " + lista[4]);
+		System.out.println("(JOGADOR 2): " + lista[5] + " | " + lista[6] + " | " + lista[7] + " | " + lista[8] + " | " + lista[9]);
+		System.out.println("(JOGADOR 3): " + lista[10] + " | " + lista[11] + " | " + lista[12] + " | " + lista[13] + " | " + lista[14]);
+		System.out.println("(JOGADOR 4): " + lista[15] + " | " + lista[16] + " | " + lista[17] + " | " + lista[18] + " | " + lista[19]);
 	}
 }
